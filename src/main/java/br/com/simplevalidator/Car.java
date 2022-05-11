93ca76c48b8b0b2641263940fb2d1c54931d03e1
jeanls <jean.dev@outlook.com>
2022-05-10 21:06:37 -0300

    feat: remove unused files

--- src/main/java/br/com/simplevalidator/Car.java
+++ src/main/java/br/com/simplevalidator/Car.java
@@ -15,12 +15,6 @@ public class Car {
 
     private final int seatCount;
 
-//    public static final am.ik.yavi.core.Validator<Car> validator = ValidatorBuilder.<Car>of()
-//            .constraint(Car::getManufacturer, "manufacturer", Constraint::notNull)
-//            .constraint(Car::getLicensePlate, "licensePlate", c -> c.notNull().greaterThanOrEqual(2).lessThanOrEqual(14))
-//            .constraint(Car::getSeatCount, "seatCount", c -> c.greaterThanOrEqual(2))
-//            .build();
-
     public Car(String manufacturer, String licencePlate, int seatCount) {
         this.manufacturer = manufacturer;
         this.licensePlate = licencePlate;
